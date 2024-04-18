2a78bb7715fd87a53a858696237bf006f7e7805b
pythonzoa <159130580+pythonzoa@users.noreply.github.com>
2024-04-18 15:44:42 +0900

    Add files via upload

---
+++ newsBonmun.py
@@ -0,0 +1,148 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import smtplib
+from datetime import datetime, timedelta
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import time
+import re
+
+keywords = ["아시아나항공", "제주항공", "이스타항공", "에어인천", "에어프레미아", "티웨이항공"]
+# 현재 시간을 YYYY-MM-DD HH:MM 형식으로 포맷
+current_time = datetime.now().strftime('%y/%m/%d %H:%M')
+def extract_article_content(url):
+    """
+    주어진 URL에서 기사의 본문을 추출하여 반환합니다.
+
+    Args:
+    url (str): 기사의 웹 주소.
+
+    Returns:
+    str: 기사 본문의 텍스트. 본문을 찾을 수 없는 경우, 비어있는 문자열을 반환합니다.
+    """
+    try:
+        response = requests.get(url)
+        if response.status_code != 200:
+            return ""
+
+        soup = bs(response.content, 'html.parser')
+        article_body = soup.find('div', class_='article-body')
+
+        if article_body:
+            paragraphs = article_body.find_all('p')
+            article_text = '\n'.join(paragraph.text for paragraph in paragraphs)
+            return article_text
+        else:
+            return ""
+    except Exception as e:
+        print(f"Error extracting article content: {e}")
+        return ""
+
+def is_keyword_in_article(url, keyword):
+    """
+    주어진 URL의 기사 본문에서 지정된 키워드의 존재 여부를 검사합니다.
+
+    Args:
+    url (str): 기사의 웹 주소.
+    keyword (str): 검사할 키워드.
+
+    Returns:
+    bool: 키워드가 기사 본문에 존재하면 True, 그렇지 않으면 False를 반환합니다.
+    """
+    article_text = extract_article_content(url)
+    return keyword in article_text
+
+def get_naver_news(keyword):
+    url = f"https://search.naver.com/search.naver?where=news&query={keyword}"
+    response = requests.get(url)
+    soup = bs(response.text, 'html.parser')
+    now = datetime.now()
+    articles_list = []
+
+    articles = soup.find_all('div', class_='news_area')
+    for article in articles:
+        title_tag = article.find('a', class_='news_tit')
+        title = title_tag.get('title')
+        link = title_tag.get('href')
+        date_tag = article.find('span', class_='info')
+        date_text = date_tag.text
+
+        if "시간 전" in date_text or "분 전" in date_text:
+            hours_ago = int(re.search(r'\d+', date_text).group())
+            article_time = now - timedelta(hours=hours_ago)
+            if article_time > now - timedelta(hours=12):
+                if is_keyword_in_article(link, '화물'):
+                    articles_list.append((keyword, title, link, date_text))
+
+    return articles_list
+
+
+def MarinaNews(keywords, get_naver_news, send_email):
+    # 각 키워드에 대해 가져온 기사들을 담는 리스트
+    articles = [article for keyword in keywords for article in get_naver_news(keyword)]
+
+    # 기사의 제목과 링크를 담는 세트를 초기화하여 중복을 제거
+    unique_articles = set()
+    for article in articles:
+        # 기사의 (제목, 링크) 튜플을 세트에 추가
+        unique_articles.add((article[1], article[2]))
+
+    # 시작 HTML 구성
+    html = """
+    <html>
+    <head>
+    <style>
+      .article { margin-bottom: 20px; }
+      a { text-decoration: none; color: blue; font-size: 14px;line-height: 2;}
+      a:hover { text-decoration: underline; }
+    </style>
+    </head> 
+    <body>
+    <h2>최신 기사 LIST</h2>
+    """
+
+    # 기사 링크와 번호를 초기화
+    article_links = []
+    article_counter = 1
+
+    # 중복이 제거된 기사들을 순회하면서 HTML에 추가
+    for title, link in unique_articles:
+        article_link = f'{article_counter}. <a href="{link}">{title}</a>'
+        article_links.append(article_link)
+        article_counter += 1
+
+    # 기사 링크들을 HTML에 추가
+    html += '<br>'.join(article_links)
+    html += "</body></html>"
+
+    # 이메일을 발송하는 함수를 호출
+    send_email(html)
+
+def send_email(html_content):
+    # Gmail 계정 정보
+    sender_email = "hwijunjang@koreanair.com"  # 발신자 이메일 주소
+    app_password = "aaig gxux eokv kmjv"  # Gmail 애플리케이션 비밀번호
+    # 복수의 수신자 이메일 주소들
+    receiver_emails = ["hwijunjang@koreanair.com", "onlyhalfgp@gmail.com"]  # 수신자 이메일 주소 리스트
+
+    # 이메일 메시지 설정
+    msg = MIMEMultipart('alternative')
+    msg['From'] = sender_email
+    msg['To'] = ", ".join(receiver_emails)  # 수신자 이메일 주소들을 콤마로 구분하여 설정
+    msg['Subject'] = f"Project Maria 관련 최신 뉴스-{current_time}"
+
+    msg.attach(MIMEText(html_content, 'html'))
+
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(sender_email, app_password)
+        server.send_message(msg)
+        server.quit()
+        print("이메일이 성공적으로 발송되었습니다.")
+    except Exception as e:
+        print(f"이메일 전송에 실패했습니다: {e}")
+
+
+MarinaNews(keywords, get_naver_news, send_email)
+
